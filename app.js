02420d7790694d3c742cfeaa6675b3b06fb6176e
timothyrusso <russotimothy@live.it>
2022-05-19 08:33:55 +0200

    feat: errors classes and app limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const helmet = require('helmet');
 const mongoose = require("mongoose");
-const { mongoDbAdress } = require("./utils/constants");
+const { mongoDbAdress, limiter } = require("./utils/constants");
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -8,6 +9,13 @@ const { PORT = 3000 } = process.env;
 mongoose.connect(mongoDbAdress, {
   useNewUrlParser: true,
 });
+
+app.use(limiter);
+
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`); // eslint-disable-line no-console
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,21 @@
+const rateLimit = require("express-rate-limit");
+
+const REQUEST_SUCCEDED = 200;
+const RESOURCE_CREATED = 201;
+const INTERNAL_SERVER_ERROR = 500;
+
 const mongoDbAdress = "mongodb://localhost:27017/newsdb";
 
+const limiter = rateLimit({
+  // Limit repeated requests to public APIs and/or endpoints
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+
 module.exports = {
+  REQUEST_SUCCEDED,
+  RESOURCE_CREATED,
+  INTERNAL_SERVER_ERROR,
   mongoDbAdress,
+  limiter,
 };
