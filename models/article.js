e25f9fa8d2c64b02cbb27adf07c377c8f6b85bdc
timothyrusso <russotimothy@live.it>
2022-05-19 08:21:10 +0200

    feat: user and article models

---
+++ models/article.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: [true, "url required"],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "This field must be a link.",
+    },
+  },
+  image: {
+    type: String,
+    required: [true, "url required"],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "This field must be a link.",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+});
+
+articleSchema.methods.toJSON = function () {
+  // eslint-disable-line
+  const { owner, ...obj } = this.toObject(); // eslint-disable-line
+  return obj;
+};
+
+module.exports = mongoose.model("article", articleSchema);
---
+++ models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "email required"],
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "This field must be an email.",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+
+        return user;
+      });
+    });
+};
+
+userSchema.methods.toJSON = function () {
+  // eslint-disable-line
+  const { password, ...obj } = this.toObject(); // eslint-disable-line
+  return obj;
+};
+
+module.exports = mongoose.model("user", userSchema);
