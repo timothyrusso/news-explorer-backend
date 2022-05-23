932a3bc4b389420658e4af61270c3dbe5b5b73fd
timothyrusso <russotimothy@live.it>
2022-05-23 06:45:07 +0200

    feat: middlewares for auth, logging and validations

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,22 +2,35 @@ const Article = require("../models/article");
 const NotFoundError = require("../errors/not-found-err");
 const ForbiddenError = require("../errors/forbidden-err");
 const BadRequestError = require("../errors/bad-request-err");
-const {
-  REQUEST_SUCCEDED,
-  RESOURCE_CREATED,
-} = require("../utils/constants");
+const { REQUEST_SUCCEDED, RESOURCE_CREATED } = require("../utils/constants");
 
-const getUserArticles = (req, res, next) => Article.find({})
-  .then((articles) => res.status(REQUEST_SUCCEDED).send(articles))
-  .catch(next);
+const getUserArticles = (req, res, next) =>
+  Article.find({})
+    .then((articles) => res.status(REQUEST_SUCCEDED).send(articles))
+    .catch(next);
 
 const saveArticle = (req, res, next) => {
-  const { keyword, title, text, date, source, link, image   } = req.body;
-  Article.create({ keyword, title, text, date, source, link, image, owner: req.user._id })
+  const { keyword, title, text, date, source, link, image } = req.body;
+  Article.create({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner: req.user._id,
+  })
     .then((articles) => res.status(RESOURCE_CREATED).send(articles))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new BadRequestError(`${Object.values(err.errors).map((error) => error.message).join(', ')}`));
+      if (err.name === "ValidationError") {
+        next(
+          new BadRequestError(
+            `${Object.values(err.errors)
+              .map((error) => error.message)
+              .join(", ")}`
+          )
+        );
       } else {
         next(err);
       }
@@ -26,12 +39,14 @@ const saveArticle = (req, res, next) => {
 
 const deleteArticle = (req, res, next) => {
   Card.findById(req.params.articleId)
-    .orFail(() => new NotFoundError('Article ID not found'))
+    .orFail(() => new NotFoundError("Article ID not found"))
     .then((article) => {
       if (!article.owner.equals(req.user._id)) {
-        next(new ForbiddenError('Yoy cannot delete someone else\'s article')); // cannot delete the article if you are not the owner
+        next(new ForbiddenError("Yoy cannot delete someone else's article")); // cannot delete the article if you are not the owner
       } else {
-        Article.deleteOne(article).then(() => res.status(REQUEST_SUCCEDED).send({ data: article }));
+        Article.deleteOne(article).then(() =>
+          res.status(REQUEST_SUCCEDED).send({ data: article })
+        );
       }
     })
     .catch(next);
---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+const ForbiddenError = require('../errors/forbidden-err');
+
+// Middleware that check on every request if the user have the authorization
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new ForbiddenError('Authorization Required'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'casual-secret-key');
+  } catch (err) {
+    return next(new ForbiddenError('Authorization Required'));
+  }
+
+  req.user = payload; // assigning the payload to the request object
+
+  return next(); // sending the request to the next middleware
+};
