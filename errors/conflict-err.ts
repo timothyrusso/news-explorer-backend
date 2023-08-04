0a8e460b2380698fbca65153bf0da251cf06de41
Timothy Russo <russotimothy@live.it>
2023-08-04 11:30:21 +0200

    feat: migrate to typescript the confict error module

--- errors/conflict-err.ts
+++ errors/conflict-err.ts
@@ -1,8 +1,10 @@
 class ConflictError extends Error {
-  constructor(message) {
+  statusCode: number;
+
+  constructor(message?: string) {
     super(message);
     this.statusCode = 409;
   }
 }
 
-module.exports = ConflictError;
+export default ConflictError;
