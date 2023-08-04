ec17582f0536c4add91a86be8680ec891873cc07
Timothy Russo <russotimothy@live.it>
2023-08-04 16:17:17 +0200

    feat: migrate to typescript the app file

--- app.ts
+++ app.ts
@@ -1,20 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const routes = require('./routes');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const error = require('./middlewares/error');
-require('dotenv').config();
-const { limiter, FRONTEND_URL } = require('./utils/constants');
+import express, { RequestHandler, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import { errors } from 'celebrate';
+import routes from './routes';
+import { requestLogger, errorLogger } from './middlewares/logger';
+import error from './middlewares/error';
+import { config as dotenvConfig } from 'dotenv';
+import { limiter, FRONTEND_URL } from './utils/constants';
+
+dotenvConfig();
 
 const app = express();
-const { PORT = 3000, MONGO_URL, NODE_ENV } = process.env;
 
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/newsdb', {
-  useNewUrlParser: true,
-}, (err) => {
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/newsdb', NODE_ENV = 'development' } = process.env;
+
+mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/newsdb', (err) => {
   if (err) {
     console.log('Error connecting to MongoDB:', err);
   } else {
@@ -23,6 +24,7 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27
 });
 
 app.use(express.json()); // To parse the incoming requests with JSON payloads
+
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 app.use(helmet());
@@ -31,7 +33,7 @@ app.use(requestLogger); // enabling the request logger
 
 app.use(limiter);
 
-app.use((req, res, next) => {
+const corsMiddleware: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
   res.header(
     'Access-Control-Allow-Origin',
     NODE_ENV === 'production' ? FRONTEND_URL : 'http://localhost:3000/',
@@ -42,9 +44,12 @@ app.use((req, res, next) => {
   );
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
-});
+};
+
+app.use(corsMiddleware);
 
 app.use(cors());
+
 app.options('*', cors()); // Enable requests for all routes
 
 app.use(routes);
